3f6fec25d3bd5f0f21d497a4b1b3863dbc345514
CodeBear <249900679@qq.com>
2018-01-25 17:22:29 +0800

    [修改] time helper

--- application/helpers/cb_time_helper.php
+++ application/helpers/cb_time_helper.php
@@ -1,12 +1,12 @@
 <?php
 
 //1秒
-const SECOND = 1;
+define('SECOND', 1);
 //1分钟
-const MINUTE = self::SECOND * 60;
+define('MINUTE', SECOND * 60);
 //1小时
-const HOUR = self::MINUTE * 60;
+define('HOUR', MINUTE * 60);
 //1天
-const DAY = self::HOUR * 24;
+define('DAY', HOUR * 60);
 //1周
-const WEEK = self::DAY * 7;
+define('WEEK', DAY * 60);
